708edf89af03212e221dc718035f2227876f94fe
Franklin Lugo <franklinlugo2805@gmail.com>
2018-03-22 12:29:15 -0400

    add: main component

--- src/entries/index.js
+++ src/entries/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { App } from '../components/App';
 
-const el = React.createElement('h1', null, 'hello world');
-
-ReactDOM.render(el, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById('app'))
