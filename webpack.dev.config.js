a0c74162afba42b7baac8b99eb21fa958ecb68b0
Franklin Lugo <franklinlugo2805@gmail.com>
2018-04-12 03:09:19 -0400

    add: eslint and prettier

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,44 +1,44 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    entry: {
-        main: path.resolve(__dirname, 'src/entries/index.js'),
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'js/[name].js'
-    },
-    devtool: 'eval-source-map',
-    devServer: {
-        port: 8000,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /(node_modules)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['env', 'react', 'stage-2']
-                    }
-                },
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            },
-            {
-                test: /\.(jpg|png|gif|svg)$/,
-                use: {
-                    loader: 'url-loader',
-                    options: {
-                        limit: 1000000,
-                        fallback: 'file-loader',
-                        name: 'images/[name].[ext]',
-                    }
-                }
-            },
-        ]
-    }
-}
+  entry: {
+    main: path.resolve(__dirname, "src/entries/index.js")
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/[name].js"
+  },
+  devtool: "eval-source-map",
+  devServer: {
+    port: 8000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["env", "react", "stage-2"]
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(jpg|png|gif|svg)$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            limit: 1000000,
+            fallback: "file-loader",
+            name: "images/[name].[ext]"
+          }
+        }
+      }
+    ]
+  }
+};
