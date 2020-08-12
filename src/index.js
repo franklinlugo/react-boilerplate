38145735c7f3843422b13eb55c1e6362f9fe7156
franklinlugo <franklinlugo2805@gmail.com>
2020-08-12 20:08:14 -0300

    chore: jest config

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-function App() {
-  return (
-    <div>
-      <h1>App</h1>
-    </div>
-  );
-}
+import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('app'));
