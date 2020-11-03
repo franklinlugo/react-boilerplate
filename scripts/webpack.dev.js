14379c81c0434139168a017087f4905f9db42a2a
Franklin Lugo <franklinlugo2805@gmail.com>
2020-11-03 12:09:59 -0300

    chore: dev-dependencies

--- scripts/webpack.dev.js
+++ scripts/webpack.dev.js
@@ -3,7 +3,7 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'development',
-  devtool: 'cheap-module-source-map',
+  devtool: 'inline-source-map',
   devServer: {
     port: 3000,
     historyApiFallback: true,
