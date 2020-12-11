83f6432a32ed45bbd425bfea834d06b88dfe2492
Franklin Lugo <franklinlugo2805@gmail.com>
2020-12-11 05:45:06 -0300

    fix: publicPath value in webpack config

--- scripts/webpack.dev.js
+++ scripts/webpack.dev.js
@@ -5,7 +5,8 @@ module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
-    port: 3000,
     historyApiFallback: true,
+    port: 3000,
+    publicPath: '/',
   },
 });
