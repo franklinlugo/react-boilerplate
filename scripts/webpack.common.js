0084760b905a1649d8f06791cfbd361c264a4424
franklinlugo <franklinlugo2805@gmail.com>
2021-01-13 12:57:22 -0300

    update: dependencies and structure

--- scripts/webpack.common.js
+++ scripts/webpack.common.js
@@ -4,45 +4,42 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 const ErrorOverlayPlugin = require('error-overlay-webpack-plugin');
 const dotenv = require('dotenv').config({ path: path.join(__dirname, '.env') });
 
-const javascriptRules = {
-  test: /\.jsx?$/,
-  exclude: /node_modules/,
-  use: {
-    loader: 'babel-loader',
-  },
-};
-
-const htmlRules = {
-  test: /\.html$/,
-  use: [{ loader: 'html-loader' }],
-};
-
-const fileRules = {
-  test: /\.(jpe?g|png|gif|svg)$/i,
-  use: [
-    {
-      loader: 'file-loader',
-    },
-  ],
-};
-
-const cssRules = {
-  test: /\.css$/,
-  use: ['style-loader', 'css-loader'],
-};
-
-const rules = [javascriptRules, htmlRules, fileRules, cssRules];
-
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, '../dist'),
     filename: '[name].js',
   },
-  module: { rules },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [{ loader: 'html-loader' }],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
   plugins: [
     new HtmlWebPackPlugin({
-      template: './src/index.html',
+      template: './public/index.html',
       filename: 'index.html',
     }),
     new ErrorOverlayPlugin(),
